1e22a8ebb4dccedc2ee0e2f22099fd5d476b1f81
sebiseba2022 <chitufloriansebastian@gmail.com>
2026-01-11 23:22:49 +0200

    solved the problem. de rezolvat cu pozele daca sunt mai mari de 1mb

--- src/main/java/com/parking/parkinglot/entities/User.java
+++ src/main/java/com/parking/parkinglot/entities/User.java
@@ -1,44 +1,51 @@
 package com.parking.parkinglot.entities;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
 
-import java.util.LinkedHashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
-@Table(name = "user")
+@Table(name = "users")
 public class User {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "username")
-    private String username;
-
-    @Column(name = "password")
+    @NotNull(message = "Password is required")
+    @Size(min = 6, max = 100, message = "Password must be at least 6 characters")
+    @Column(name = "password", nullable = false, length = 100)
     private String password;
 
-    @Column(name = "email")
+    @NotNull(message = "Email is required")
+    @Email(message = "Email must be valid")
+    @Size(max = 100, message = "Email must not exceed 100 characters")
+    @Column(name = "email", nullable = false, length = 100, unique = true)
     private String email;
 
+    @NotNull(message = "Username is required")
+    @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
+    @Column(name = "username", nullable = false, length = 50, unique = true)
+    private String username;
+
+    @OneToMany(mappedBy = "owner", orphanRemoval = true)
+    private List<Car> cars = new ArrayList<>();
+
+    public List<Car> getCars() {
+        return cars;
+    }
+
+    public void setCars(List<Car> cars) {
+        this.cars = cars;
+    }
 
     public String getPassword() {
         return password;
     }
-
-    @OneToMany(mappedBy = "owner", orphanRemoval = true)
-    private Set<Car> cars = new LinkedHashSet<>();
-
-
-    public Set<Car> getCars() {
-        return cars;
-    }
-
-    public void setCars(Set<Car> cars) {
-        this.cars = cars;
-    }
-
 
     public void setPassword(String password) {
         this.password = password;
@@ -65,7 +72,6 @@ public class User {
     }
 
     public void setId(Long id) {
-        this.id = id;
-    }
-
+        this.id=id;
+}
 }
